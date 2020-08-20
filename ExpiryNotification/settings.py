c4909a8788e1da1e45c5f6f56996fe60d0181dd0
Vivek <vickygupta1996@gmail.com>
2020-08-21 00:06:19 +0530

    Allowed Hosts

--- ExpiryNotification/settings.py
+++ ExpiryNotification/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['EXPIRY_NOTIFICATION_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vivekgupta16.pythonanywhere.com']
 
 
 # Application definition
